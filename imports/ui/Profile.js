7b9ac7ab5e4de3da1a99d05ec2bda9e927cf47ca
Jacquiline Chapoloko <jacchapoloko@gmail.com>
2018-09-28 15:32:48 +0200

    add footer and profile page

---
+++ imports/ui/Profile.js
@@ -0,0 +1,66 @@
+import React, {Component } from 'react';
+import Image from 'react-image-resizer';
+import Footer from './Footer.js';
+
+
+
+class Profile extends Component {
+
+	render(){
+		return (
+		<div>
+		<div class='row'>
+				<div class="col s12">
+					
+				</div>
+		</div>
+		<div class="row">
+			<div class="col s4" id='user-pic'>
+			      <Image src="images/user.png" width={300} height={300} alt="" 
+				class="circle responsive-img"/>
+			 </div>
+			    
+			<div class="col s8">
+				  <div class="section" id='user-info'>
+				    <h5 id='blue'>User Name</h5>
+				    
+					Buyer | Seller<br/>
+					Lusaka, Zambia<br/> 
+					+26 123 456 789<br/> 
+				    	<br/>
+					 <button class="btn waves-effect waves-light light-blue lighten-1" type="submit" name="action">Settings
+					    <i class="material-icons right">settings</i>
+					  </button>
+				  </div>
+				  <div class="divider"></div>
+   			</div>
+				</div>
+				<div class='row'>
+				<div class="col s12">
+				<h2 id='blue'>Inbox</h2>
+				  <div class="section">
+				    <h5>Message 1</h5>
+				    <p>Stuff</p>
+				  </div>
+				  <div class="divider"></div>
+				  <div class="section">
+				    <h5>Message 2</h5>
+				    <p>Stuff</p>
+				  </div>
+				  <div class="divider"></div>
+				  <div class="section">
+				    <h5>Message 3</h5>
+				    <p>Stuff</p>
+				  </div>
+			
+				</div>
+		</div>
+		<Footer/>
+		</div>
+		
+		);
+
+	}
+};
+
+export default Profile
